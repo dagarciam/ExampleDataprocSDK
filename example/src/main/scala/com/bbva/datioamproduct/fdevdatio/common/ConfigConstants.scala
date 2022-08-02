6c4988936c66f9f5cf8e7b77fdb5e5c9d6e85e6a
Daniel García <dagarciam@minsait.com>
2022-08-02 17:30:47 -0500

    update IOUtils

--- example/src/main/scala/com/bbva/datioamproduct/fdevdatio/common/ConfigConstants.scala
+++ example/src/main/scala/com/bbva/datioamproduct/fdevdatio/common/ConfigConstants.scala
@@ -6,14 +6,23 @@ object ConfigConstants {
   val IncludeMetadataFields: String = s"$Options.includeMetadataFields"
   val IncludeDeletedFields: String = s"$Options.includeDeletedFields"
   val PartitionOverwriteMode: String = s"$Options.partitionOverwriteMode"
+  val CoalesceNumber: String = s"$Options.coalesce"
 
+  val Schema: String = "schema"
   val Path: String = "path"
   val Type: String = "type"
-  val SchemaPath: String = "schema.path"
-  val PartitionOverwriteModeString: String = "partitionOverwriteMode"
-  val Delimiter: String = "delimiter"
-  val Header: String = "header"
+  val SchemaPath: String = s"$Schema.path"
+  val OverrideSchema: String = s"$Schema.overrideSchema"
+  val MergeSchema: String = s"$Schema.mergeSchema"
+  val PartitionOverwriteModeString: String = s"$Options.partitionOverwriteMode"
+  val Delimiter: String = s"$Options.delimiter"
+  val Header: String = s"$Options.header"
   val Partitions: String = "partitions"
   val Mode: String = "mode"
+
+  val DelimiterOption: String = "delimiter"
+  val HeaderOption: String = "header"
+  val OverrideSchemaOption: String = "overrideSchema"
+  val MergeSchemaOption: String = "mergeSchema"
 
 }
